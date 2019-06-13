7c037a9e01cbaf5fef22010b9cb9c94b7ca62938
Colton-Bishop <cmbishop@princeton.edu>
2019-06-13 16:18:46 -0400

    Intro Framework

--- wengga/settings.py
+++ wengga/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['wengga.herokuapp.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'language.apps.LanguageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'wengga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
